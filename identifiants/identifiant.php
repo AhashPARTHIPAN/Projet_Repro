69b246126baa8cf8fdbe7b3f05b04eb3621e39f6
AhashPARTHIPAN <163518335+AhashPARTHIPAN@users.noreply.github.com>
2025-02-06 14:38:14 +0100

    Create identifiant.php

---
+++ identifiants/identifiant.php
@@ -0,0 +1,6 @@
+<?php 
+    $host = 'localhost';          // Hôte MySQL
+    $dbname = 'nom_de_la_base';       // Nom de la base de données
+    $username = 'nom_de_l_utilisateur';     // Nom d'utilisateur
+    $password = 'mdp_de_l_utilisateur';   // Mot de passe de l'utilisateur
+?>
